bc7e28d4da46eb14207f80291ce91509280c829b
arfakhan <khanarfa8879@gmail.com>
2024-04-05 11:46:01 +0530

    core layout done

---
+++ src/pages/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import CardsList from '../components/CardsList'
+
+export default function HomeScreen() {
+  return (
+    <>
+    <Navbar />
+    <CardsList />
+    </>
+  )
+}
