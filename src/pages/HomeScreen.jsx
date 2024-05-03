100b8464223e44db0ab5cbeaa692490691c980ab
arfakhan <khanarfa8879@gmail.com>
2024-05-03 18:45:30 +0530

    map added again

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -105,7 +105,16 @@ export default function HomeScreen() {
     <>
       <Navbar handlesearch={handleSearch} />
       {render()}
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30141.10166934225!2d72.95332365871046!3d19.21101992551151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b947dc7067ef%3A0x6eaba8fa805ec81!2sItdaksh%20Education!5e0!3m2!1sen!2sin!4v1714741380282!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30141.10166934225!2d72.95332365871046!3d19.21101992551151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b947dc7067ef%3A0x6eaba8fa805ec81!2sItdaksh%20Education!5e0!3m2!1sen!2sin!4v1714741380282!5m2!1sen!2sin"
+      width="600"
+      height="450"
+      style={{ border: 0 }}
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+      title="Google Map"
+    ></iframe>
     </>
   );
 }
